a02f54ed3f55dc97877cadb564f14141bbb8d7df
Denis Cheung <d.s.h.cheung@gmail.com>
2015-12-02 13:32:50 +0800

    solution

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,28 @@
-angular.module('InfamousCriminals', []);
+angular
+  .module('InfamousCriminals', ['ui.router'])
+  .config(CriminalRouter);
+
+function CriminalRouter($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider.otherwise("/");
+
+  $stateProvider
+  .state('index', {
+    url: '/',
+    templateUrl: 'list.html'
+  })
+  .state('new', {
+    url: '/new',
+    templateUrl: 'new.html'
+  })
+  .state('about', {
+    url: '/about',
+    templateUrl: 'about.html'
+  })
+  .state('show', {
+    url: '/criminals/:id',
+    templateUrl: 'show.html'
+  });
+
+
+}
